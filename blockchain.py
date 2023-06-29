f20e0341d12155580a5d1f7877b76189a06e6f2d
Nilay Sharma <nilaysharma642@gmail.com>
2023-06-29 19:00:00 +0530

    add server config

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,8 @@ import json
 from time import time
 from uuid import uuid4
 
+from flash import Flask, jsonify
+
 class Blockchain (object):
   def __init__(self):
     self.chain = []
@@ -93,3 +95,31 @@ class Blockchain (object):
     guess = f'{last_proof}{proof}'.encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
     return guess_hash[:4] == "0000"
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('_', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+  return "We'll mine a new Block"
+
+@app.route('/transaction/new', methods=["POST"])
+def new_transaction():
+  return "We'll add a new transaction"
+
+@app.route('/chain', methods=["GET"])
+def full_chain():
+  response = {
+    'chain': blockchain.chain,
+    'length': len(blockchain.chain),
+  }
+  return jsonify(response), 200
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=5000)
