fa0b59492c7ad20d0f4c7bace005bc18697f51f0
Nilay Sharma <nilaysharma642@gmail.com>
2023-07-07 15:30:00 +0530

    logic for consensus

--- blockchain.py
+++ blockchain.py
@@ -49,7 +49,39 @@ class Blockchain (object):
     
     return True
   
+  def resolve_conflicts(self):
+    """
+    This is out Consensus Algorithm, it resolves conflicts
+    by replacing our chain with the longest one in the network
 
+    : return: <bool> True is our chain was replaced, False if not
+    """
+
+    neighbors = self.nodes
+    new_chain = None
+
+    # We're only looking for chains longer than ours
+    max_length = len(self.chain)
+
+    # Grab and verify the chains from all the nodes in our network
+    for node in neighbors:
+      response = requests.get(f'http://{node}/chain')
+
+      if response.status_code == 200:
+        length = response.json()['length']
+        chain = response.json()['chain']
+
+        # Check if the length is longer and the chain is valid
+        if length > max_length and self.valid_chain(chain):
+          max_length = length
+          new_chain = chain
+
+    # Replace our chain if we discovered a new, valid chain longer than ours
+    if new_chain:
+      self.chain = new_chain
+      return True
+
+    return False 
 
   def new_block(self, proof, previous_hash=None):
     '''
